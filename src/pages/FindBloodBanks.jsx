48c71c0207aa3dfd00ec99e7a68ede9c0e6132ee
manoj <manojkailash1@gmail.com>
2025-12-05 14:51:25 +0530

    Fix: Add simple FindBloodBanks component

---
+++ src/pages/FindBloodBanks.jsx
@@ -0,0 +1,14 @@
+﻿import { useState } from 'react';
+
+function FindBloodBanks() {
+  return (
+    <div className="container">
+      <h1 style={{ color: 'white', marginBottom: '30px' }}>Find Nearby Blood Banks</h1>
+      <div className="card">
+        <p>Map feature coming soon!</p>
+      </div>
+    </div>
+  );
+}
+
+export default FindBloodBanks;
